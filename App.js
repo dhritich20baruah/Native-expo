dc8a2ba12e7a5bf546073b1e5aecc6e28fe8733b
Dhriti <dhriti.baruah20@gmail.com>
2024-03-28 13:14:58 +0530

    input text

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [text, setText] = useState('')
+  const [count, setCount] = useState(0)
+
+  const handleInputChange = (inputText) => {
+    setText(inputText)
+  }
+  const handlePress = () => {
+    Alert.alert("Input Value", text)
+  }
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello World!!</Text>
+      <TextInput onChangeText={handleInputChange} value={text} placeholder='Enter text here' style={styles.input}/>
+      <Button title="submit" onPress={handlePress}/>
+      <Text style={styles.text}>{count}</Text>
+      <Button title="increment" onPress={increment}/>
+      <Button title="decrement" onPress={decrement}/>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -17,4 +40,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'red',
+    fontSize: 25,
+    margin: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    width: 100
+  }
 });
